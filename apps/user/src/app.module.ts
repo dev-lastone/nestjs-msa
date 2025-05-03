c96d512c362bbe925ab2b67f0ff3b416106ba31f
jjong <akrTmwk@gmail.com>
2025-05-03 20:06:43 +0900

    chore: docker-compose user env_file 설정

--- apps/user/src/app.module.ts
+++ apps/user/src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        HTTP_PORT: Joi.number().required(),
+        DB_URL: Joi.string().required(),
+      }),
+    }),
+    UserModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
