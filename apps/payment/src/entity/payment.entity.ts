8aeaf286af1a0609c8d1f883f00d66fa74c652e4
jjong <akrTmwk@gmail.com>
2025-05-04 17:29:34 +0900

    feat: payment entity

---
+++ apps/payment/src/entity/payment.entity.ts
@@ -0,0 +1,55 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+export enum PaymentStatus {
+  PENDING = 'pending',
+  REJECTED = 'rejected',
+  APPROVED = 'approved',
+}
+
+export enum PaymentMethod {
+  CREDIT_CARD = 'credit_card',
+}
+
+export enum NotificationStatus {
+  PENDING = 'pending',
+  SENT = 'sent',
+}
+
+@Entity()
+export class Payment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    enum: PaymentStatus,
+    default: PaymentStatus.PENDING,
+  })
+  paymentStatus: PaymentStatus;
+
+  @Column({
+    enum: PaymentMethod,
+    default: PaymentMethod.CREDIT_CARD,
+  })
+  paymentMethod: PaymentMethod;
+
+  @Column()
+  cardNumber: string;
+
+  @Column()
+  expiryYear: string;
+
+  @Column()
+  expiryMonth: string;
+
+  @Column()
+  birthOrRegistration: string;
+
+  @Column()
+  passwordTwoDigit: string;
+
+  @Column({
+    enum: NotificationStatus,
+    default: NotificationStatus.PENDING,
+  })
+  notificationStatus: NotificationStatus;
+}
