69a1962c614d8abe526829aaee1b4aec76293ce7
jjong <akrTmwk@gmail.com>
2025-05-04 15:19:44 +0900

    feat: product AppModule config, typeorm 설정 추가

--- apps/product/src/app.module.ts
+++ apps/product/src/app.module.ts
@@ -1,7 +1,27 @@
 import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DB_URL: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.getOrThrow('DB_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    ProductModule,
+  ],
 })
 export class AppModule {}
