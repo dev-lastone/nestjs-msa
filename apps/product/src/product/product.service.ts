d1d15dfceeba9544f96c60ba901cecadf0776ec9
jjong <akrTmwk@gmail.com>
2025-05-04 15:34:11 +0900

    feat: product entity 구성

--- apps/product/src/product/product.service.ts
+++ apps/product/src/product/product.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Product } from './entity/product.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class ProductService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepo: Repository<Product>,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
   }
