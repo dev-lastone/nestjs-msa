7ba90b9388d738d4f415dcad63d361a0ee6e5f04
jjong <akrTmwk@gmail.com>
2025-05-04 17:11:39 +0900

    feat: apps order entity 구성

---
+++ apps/order/src/order/entity/order.entity.ts
@@ -0,0 +1,51 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Payment, PaymentSchema } from './payment.entity';
+import {
+  DeliveryAddress,
+  DeliveryAddressSchema,
+} from './delivery-address.entity';
+import { Product, ProductSchema } from './product.entity';
+import { Customer, CustomerSchema } from './customer.entity';
+
+export enum OrderStatus {
+  PENDING = 'pending',
+  PAYMENT_CANCELLED = 'paymentCancelled',
+  PAYMENT_FAILED = 'paymentFailed',
+  PAYMENT_PROCESSED = 'paymentProcessed',
+  DELIVERY_STARTED = 'deliveryStarted',
+  DELIVERY_DONE = 'deliveryDone',
+}
+
+@Schema()
+export class Order extends Document {
+  @Prop({
+    type: CustomerSchema,
+    required: true,
+  })
+  customer: Customer;
+
+  @Prop({
+    type: ProductSchema,
+    required: true,
+  })
+  products: Product[];
+
+  @Prop({
+    type: DeliveryAddressSchema,
+    required: true,
+  })
+  deliveryAddress: DeliveryAddress;
+
+  @Prop({
+    enum: OrderStatus,
+  })
+  status: OrderStatus;
+
+  @Prop({
+    type: PaymentSchema,
+    required: true,
+  })
+  payment: Payment;
+}
+
+export const OrderSchema = SchemaFactory.createForClass(Order);
