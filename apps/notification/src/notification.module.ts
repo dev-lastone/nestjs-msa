c28487fbfe6bb656fcb1e57fc319f56d71e325c2
jjong <akrTmwk@gmail.com>
2025-05-05 20:48:15 +0900

    chore: NotificationModule config, mongoose 설정

--- apps/notification/src/notification.module.ts
+++ apps/notification/src/notification.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { NotificationController } from './notification.controller';
 import { NotificationService } from './notification.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DB_URL: Joi.string().required(),
+      }),
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.getOrThrow('DB_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [NotificationController],
   providers: [NotificationService],
 })
