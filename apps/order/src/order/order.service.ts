f66b8a141a599acbd21966c9039438334a1b0399
jjong <akrTmwk@gmail.com>
2025-05-06 21:56:56 +0900

    feat: order userService 연결

--- apps/order/src/order/order.service.ts
+++ apps/order/src/order/order.service.ts
@@ -1,8 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class OrderService {
+  constructor(
+    @Inject('USER_SERVICE')
+    private readonly userService: ClientProxy,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async createOrder(createOrderDto: CreateOrderDto, token: string) {
+    const user = await this.getUserFromToken(token);
+  }
+
+  async getUserFromToken(token: string) {
+    const res = await lastValueFrom(
+      this.userService.send({ cmd: 'parse_bearer_token' }, { token }),
+    );
+
+    console.log(res);
   }
 }
