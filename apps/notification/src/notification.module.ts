e2b5e2e11035a36dc09b0aff89cd0e6c7cf93e1d
jjong <akrTmwk@gmail.com>
2025-05-08 01:37:39 +0900

    feat: Notification send noti

--- apps/notification/src/notification.module.ts
+++ apps/notification/src/notification.module.ts
@@ -4,6 +4,9 @@ import { NotificationService } from './notification.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Notification, NotificationSchema } from './entity/notification.entity';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ORDER_SERVICE } from '@app/common';
 
 @Module({
   imports: [
@@ -11,6 +14,8 @@ import { MongooseModule } from '@nestjs/mongoose';
       isGlobal: true,
       validationSchema: Joi.object({
         DB_URL: Joi.string().required(),
+        ORDER_HOST: Joi.string().required(),
+        ORDER_TCP_PORT: Joi.number().required(),
       }),
     }),
     MongooseModule.forRootAsync({
@@ -18,6 +23,28 @@ import { MongooseModule } from '@nestjs/mongoose';
         uri: configService.getOrThrow('DB_URL'),
       }),
       inject: [ConfigService],
+    }),
+    MongooseModule.forFeature([
+      {
+        name: Notification.name,
+        schema: NotificationSchema,
+      },
+    ]),
+    ClientsModule.registerAsync({
+      clients: [
+        {
+          name: ORDER_SERVICE,
+          useFactory: (configService: ConfigService) => ({
+            transport: Transport.TCP,
+            options: {
+              host: configService.getOrThrow('ORDER_HOST'),
+              port: configService.getOrThrow('ORDER_TCP_PORT'),
+            },
+          }),
+          inject: [ConfigService],
+        },
+      ],
+      isGlobal: true,
     }),
   ],
   controllers: [NotificationController],
--- apps/notification/src/notification.service.ts
+++ apps/notification/src/notification.service.ts
@@ -1,8 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { SendPaymentNotificationDto } from './dto/send-payment-notification.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Notification, NotificationStatus } from './entity/notification.entity';
+import { ClientProxy } from '@nestjs/microservices';
+import { ORDER_SERVICE } from '@app/common';
 
 @Injectable()
 export class NotificationService {
+  constructor(
+    @InjectModel(Notification.name)
+    private readonly notificationModel: Model<Notification>,
+    @Inject(ORDER_SERVICE)
+    private readonly orderService: ClientProxy,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async sendPaymentNotification(dto: SendPaymentNotificationDto) {
+    const notification = await this.createNotification(dto);
+
+    await this.sendEmail();
+
+    await this.updateNotificationStatus(
+      notification._id.toString(),
+      NotificationStatus.SENT,
+    );
+
+    this.sendDeliveryStartedMessage(dto.orderId);
+
+    return this.notificationModel.findById(notification._id);
+  }
+
+  sendDeliveryStartedMessage(id: string) {
+    this.orderService.emit(
+      {
+        cmd: 'delivery_started',
+      },
+      {
+        id,
+      },
+    );
+  }
+
+  async updateNotificationStatus(id: string, status: NotificationStatus) {
+    return this.notificationModel.findByIdAndUpdate(id, { status });
+  }
+
+  async sendEmail() {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
+
+  async createNotification(dto: SendPaymentNotificationDto) {
+    return await this.notificationModel.create({
+      from: 'test@test.com',
+      to: dto.to,
+      subject: '배송 시작',
+      content: `${dto.to}님, ${dto.orderId}의 배송이 시작되었습니다.`,
+    });
   }
 }
