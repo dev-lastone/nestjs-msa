f57a72ce81044dfd7acff115ab81a0a263459855
jjong <akrTmwk@gmail.com>
2025-05-08 01:32:14 +0900

    feat: msa notification tcp 등록

--- apps/notification/src/main.ts
+++ apps/notification/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { NotificationModule } from './notification.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(NotificationModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: parseInt(process.env.TCP_PORT) || 3001,
+    },
+  });
+
+  await app.startAllMicroservices();
+
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
