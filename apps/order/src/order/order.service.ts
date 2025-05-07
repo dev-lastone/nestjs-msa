ead0b7d95f00893d8e9ca8861592a6fcf2f4db7e
jjong <akrTmwk@gmail.com>
2025-05-07 12:25:52 +0900

    feat: orderService get user message

--- apps/order/src/order/order.service.ts
+++ apps/order/src/order/order.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { ClientProxy } from '@nestjs/microservices';
 import { lastValueFrom } from 'rxjs';
+import { PaymentCancelledException } from '../exception/payment-cancelled.exception';
 
 @Injectable()
 export class OrderService {
@@ -16,13 +17,28 @@ export class OrderService {
 
   async createOrder(createOrderDto: CreateOrderDto, token: string) {
     const user = await this.getUserFromToken(token);
+
+    console.log(user);
   }
 
   async getUserFromToken(token: string) {
-    const res = await lastValueFrom(
+    const jwtRes = await lastValueFrom(
       this.userService.send({ cmd: 'parse_bearer_token' }, { token }),
     );
 
-    console.log(res);
+    if (jwtRes.status === 'error') {
+      throw new PaymentCancelledException(jwtRes);
+    }
+
+    const userId = jwtRes.data.sub;
+    const userRes = await lastValueFrom(
+      this.userService.send({ cmd: 'get_user_info' }, { userId }),
+    );
+
+    if (userRes.status === 'error') {
+      throw new PaymentCancelledException(userRes);
+    }
+
+    return userRes.data;
   }
 }
