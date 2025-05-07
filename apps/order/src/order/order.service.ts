a73dbf3ddb0e02dfaa93ed228eb2c40261386d69
jjong <akrTmwk@gmail.com>
2025-05-07 14:51:13 +0900

    feat: OrderService 주문 생성

--- apps/order/src/order/order.service.ts
+++ apps/order/src/order/order.service.ts
@@ -3,6 +3,13 @@ import { CreateOrderDto } from './dto/create-order.dto';
 import { ClientProxy } from '@nestjs/microservices';
 import { lastValueFrom } from 'rxjs';
 import { PaymentCancelledException } from '../exception/payment-cancelled.exception';
+import { Product } from './entity/product.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { Order } from './entity/order.entity';
+import { Model } from 'mongoose';
+import { Customer } from './entity/customer.entity';
+import { AddressDto } from './dto/address.dto';
+import { PaymentDto } from './dto/payment.dto';
 
 @Injectable()
 export class OrderService {
@@ -11,6 +18,8 @@ export class OrderService {
     private readonly userService: ClientProxy,
     @Inject('PRODUCT_SERVICE')
     private readonly productService: ClientProxy,
+    @InjectModel('Order')
+    private readonly orderModel: Model<Order>,
   ) {}
 
   getHello(): string {
@@ -18,9 +27,24 @@ export class OrderService {
   }
 
   async createOrder(createOrderDto: CreateOrderDto, token: string) {
+    const { productIds, payment, address } = createOrderDto;
+
     const user = await this.getUserFromToken(token);
 
-    const products = await this.getProductsByIds(createOrderDto.productIds);
+    const products = await this.getProductsByIds(productIds);
+
+    const totalAmount = this.#calculateTotalAmount(products);
+
+    this.#validatePaymentAmount(totalAmount, payment.amount);
+
+    const customer = this.#createCustomer(user);
+
+    const order = await this.#createNewOrder(
+      customer,
+      products,
+      address,
+      payment,
+    );
   }
 
   async getUserFromToken(token: string) {
@@ -44,7 +68,7 @@ export class OrderService {
     return userRes.data;
   }
 
-  async getProductsByIds(productIds: string[]) {
+  async getProductsByIds(productIds: string[]): Promise<Product[]> {
     const res = await lastValueFrom(
       this.productService.send({ cmd: 'get_products_info' }, { productIds }),
     );
@@ -58,5 +82,37 @@ export class OrderService {
       name: product.name,
       price: product.price,
     }));
+  }
+
+  #calculateTotalAmount(products: Product[]) {
+    return products.reduce((acc, product) => acc + product.price, 0);
+  }
+
+  #validatePaymentAmount(totalAmount: number, paymentAmount: number) {
+    if (totalAmount !== paymentAmount) {
+      throw new PaymentCancelledException('결제 금액이 일치하지 않습니다.');
+    }
+  }
+
+  #createCustomer(user: { id: string; email: string; name: string }) {
+    return {
+      userId: user.id,
+      email: user.email,
+      name: user.name,
+    };
+  }
+
+  #createNewOrder(
+    customer: Customer,
+    products: Product[],
+    deliveryAddress: AddressDto,
+    payment: PaymentDto,
+  ) {
+    return this.orderModel.create({
+      customer,
+      products,
+      deliveryAddress,
+      payment,
+    });
   }
 }
