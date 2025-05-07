b9d47c4ca830804ebfff5dd5c73f8a0b550d26b4
jjong <akrTmwk@gmail.com>
2025-05-07 15:49:41 +0900

    feat: product create sample data api

--- apps/product/src/product/product.service.ts
+++ apps/product/src/product/product.service.ts
@@ -21,4 +21,41 @@ export class ProductService {
       },
     });
   }
+
+  async createSamples() {
+    const data = [
+      {
+        name: '사과',
+        price: 1000,
+        description: '맛있는 청주사과',
+        stock: 2,
+      },
+      {
+        name: '메론',
+        price: 2000,
+        description: '머스크 메론',
+        stock: 1,
+      },
+      {
+        name: '수박',
+        price: 3000,
+        description: '씨없는 수박',
+        stock: 10,
+      },
+      {
+        name: '브로콜리',
+        price: 2000,
+        description: '맛없는 브로콜리',
+        stock: 0,
+      },
+      {
+        name: '바나나',
+        price: 1500,
+        description: '노란 바나나',
+        stock: 3,
+      },
+    ];
+
+    await this.productRepo.save(data);
+  }
 }
