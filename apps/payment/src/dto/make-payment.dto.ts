61e19bcf3cc9c1a33003e054bc92a726b5aab43d
jjong <akrTmwk@gmail.com>
2025-05-07 17:58:57 +0900

    feat: payment 상태 변경 로직 추가

---
+++ apps/payment/src/dto/make-payment.dto.ts
@@ -0,0 +1,35 @@
+import { PaymentMethod } from '../entity/payment.entity';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class MakePaymentDto {
+  @IsNotEmpty()
+  paymentMethod: PaymentMethod;
+
+  @IsString()
+  @IsNotEmpty()
+  paymentName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  cardNumber: string;
+
+  @IsString()
+  @IsNotEmpty()
+  expiryYear: string;
+
+  @IsString()
+  @IsNotEmpty()
+  expiryMonth: string;
+
+  @IsString()
+  @IsNotEmpty()
+  birthOrRegistration: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  amount: number;
+
+  @IsString()
+  @IsNotEmpty()
+  userEmail: string;
+}
--- apps/payment/src/payment.service.ts
+++ apps/payment/src/payment.service.ts
@@ -1,8 +1,52 @@
 import { Injectable } from '@nestjs/common';
+import { Payment, PaymentStatus } from './entity/payment.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { MakePaymentDto } from './dto/make-payment.dto';
 
 @Injectable()
 export class PaymentService {
+  constructor(
+    @InjectRepository(Payment)
+    private readonly paymentRepo: Repository<Payment>,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async makePayment(payload: MakePaymentDto) {
+    let paymentId;
+    try {
+      const result = await this.paymentRepo.save(payload);
+      paymentId = result.id;
+
+      await this.processPayment();
+
+      await this.updatePaymentStatus(paymentId, PaymentStatus.APPROVED);
+
+      return this.paymentRepo.findOneBy({
+        id: paymentId,
+      });
+    } catch (e) {
+      if (paymentId) {
+        await this.updatePaymentStatus(paymentId, PaymentStatus.REJECTED);
+      }
+    }
+  }
+
+  async processPayment() {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
+
+  async updatePaymentStatus(id: string, status: PaymentStatus) {
+    await this.paymentRepo.update(
+      {
+        id,
+      },
+      {
+        paymentStatus: status,
+      },
+    );
   }
 }
