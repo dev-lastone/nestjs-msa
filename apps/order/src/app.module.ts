d23e1c496b0b899a27416d3cf52ffde628f748c8
jjong <akrTmwk@gmail.com>
2025-05-07 14:35:50 +0900

    feat: order msa product 연결

--- apps/order/src/app.module.ts
+++ apps/order/src/app.module.ts
@@ -4,7 +4,7 @@ import * as Joi from 'joi';
 import { OrderModule } from './order/order.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ClientsModule, Transport } from '@nestjs/microservices';
-import { USER_SERVICE } from '@app/common';
+import { PRODUCT_SERVICE, USER_SERVICE } from '@app/common';
 
 @Module({
   imports: [
@@ -32,6 +32,22 @@ import { USER_SERVICE } from '@app/common';
             options: {
               host: configService.getOrThrow('USER_HOST'),
               port: configService.getOrThrow('USER_TCP_PORT'),
+            },
+          }),
+          inject: [ConfigService],
+        },
+      ],
+      isGlobal: true,
+    }),
+    ClientsModule.registerAsync({
+      clients: [
+        {
+          name: PRODUCT_SERVICE,
+          useFactory: (configService: ConfigService) => ({
+            transport: Transport.TCP,
+            options: {
+              host: configService.getOrThrow('PRODUCT_HOST'),
+              port: configService.getOrThrow('PRODUCT_TCP_PORT'),
             },
           }),
           inject: [ConfigService],
