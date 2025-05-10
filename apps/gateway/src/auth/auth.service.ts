720f172335fd35da0dbac2fa08fc7ede16452c7b
jjong <akrTmwk@gmail.com>
2025-05-10 11:42:02 +0900

    feat: msa auth -> gateway auth register

--- apps/gateway/src/auth/auth.service.ts
+++ apps/gateway/src/auth/auth.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { USER_SERVICE } from '@app/common';
+import { lastValueFrom } from 'rxjs';
+import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @Inject(USER_SERVICE)
+    private readonly userMsa: ClientProxy,
+  ) {}
+
+  register(token: string, registerDto: RegisterDto) {
+    return lastValueFrom(
+      this.userMsa.send(
+        {
+          cmd: 'register',
+        },
+        {
+          ...registerDto,
+          token,
+        },
+      ),
+    );
+  }
+}
