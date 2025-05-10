c7fe3252d6196cfaed740bdd76d6de3a91c14d87
jjong <akrTmwk@gmail.com>
2025-05-10 11:41:21 +0900

    chore: gateway config, msa user 연결

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -2,9 +2,39 @@ import { Module } from '@nestjs/common';
 import { GatewayController } from './gateway.controller';
 import { GatewayService } from './gateway.service';
 import { AuthModule } from './auth/auth.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { USER_SERVICE } from '@app/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        HTTP_PORT: Joi.number().required(),
+        USER_HOST: Joi.string().required(),
+        USER_TCP_PORT: Joi.number().required(),
+      }),
+    }),
+    ClientsModule.registerAsync({
+      clients: [
+        {
+          name: USER_SERVICE,
+          useFactory: (configService: ConfigService) => ({
+            transport: Transport.TCP,
+            options: {
+              host: configService.getOrThrow('USER_HOST'),
+              port: configService.getOrThrow('USER_TCP_PORT'),
+            },
+          }),
+          inject: [ConfigService],
+        },
+      ],
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
   controllers: [GatewayController],
   providers: [GatewayService],
 })
