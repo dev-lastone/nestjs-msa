75fd28f18e4fd170815ac0b977ffc20352886cd6
jjong <akrTmwk@gmail.com>
2025-05-10 11:40:14 +0900

    chore: gateway main globalPipe ValidationPipe 설정

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { GatewayModule } from './gateway.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.HTTP_PORT ?? 3000);
 }
 bootstrap();
