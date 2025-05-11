23bb48e2d90d20b3ae21a00f439d474e6e0ef143
jjong <akrTmwk@gmail.com>
2025-05-11 17:23:09 +0900

    feat: gateway order msa 연결

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -3,9 +3,10 @@ import { GatewayController } from './gateway.controller';
 import { GatewayService } from './gateway.service';
 import { AuthModule } from './auth/auth.module';
 import { ClientsModule, Transport } from '@nestjs/microservices';
-import { USER_SERVICE } from '@app/common';
+import { ORDER_SERVICE, USER_SERVICE } from '@app/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
+import { OrderModule } from './order/order.module';
 
 @Module({
   imports: [
@@ -15,6 +16,8 @@ import * as Joi from 'joi';
         HTTP_PORT: Joi.number().required(),
         USER_HOST: Joi.string().required(),
         USER_TCP_PORT: Joi.number().required(),
+        ORDER_HOST: Joi.string().required(),
+        ORDER_TCP_PORT: Joi.number().required(),
       }),
     }),
     ClientsModule.registerAsync({
@@ -33,7 +36,24 @@ import * as Joi from 'joi';
       ],
       isGlobal: true,
     }),
+    ClientsModule.registerAsync({
+      clients: [
+        {
+          name: ORDER_SERVICE,
+          useFactory: (configService: ConfigService) => ({
+            transport: Transport.TCP,
+            options: {
+              host: configService.getOrThrow('ORDER_HOST'),
+              port: configService.getOrThrow('ORDER_TCP_PORT'),
+            },
+          }),
+          inject: [ConfigService],
+        },
+      ],
+      isGlobal: true,
+    }),
     AuthModule,
+    OrderModule,
   ],
   controllers: [GatewayController],
   providers: [GatewayService],
