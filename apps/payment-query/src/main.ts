24cb05857316b790486d3876131abe4b18735cf7
jjong <akrTmwk@gmail.com>
2025-06-05 17:22:18 +0900

    feat: payment query command msa kafka 연결

--- apps/payment-query/src/main.ts
+++ apps/payment-query/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { PaymentQueryModule } from './payment-query.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(PaymentQueryModule);
+
+  app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'payment-query',
+        brokers: ['kafka:9092'],
+      },
+      consumer: {
+        groupId: 'payment-update-consumer',
+      },
+    },
+  });
+
+  await app.init();
+
+  await app.startAllMicroservices();
+
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
