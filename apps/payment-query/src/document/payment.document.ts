ec2bc40c4dc5d297daeb90d85d07acfd354c1bab
jjong <akrTmwk@gmail.com>
2025-06-05 12:43:53 +0900

    feat: payment-query payment document

---
+++ apps/payment-query/src/document/payment.document.ts
@@ -0,0 +1,36 @@
+import { Document, ObjectId } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+enum PaymentStatus {
+  PENDING = 'PENDING',
+  COMPLETED = 'COMPLETED',
+  FAILED = 'FAILED',
+}
+
+@Schema()
+export class PaymentDocument extends Document<ObjectId> {
+  @Prop({
+    type: String,
+    required: true,
+    enum: PaymentStatus,
+    default: PaymentStatus.PENDING,
+  })
+  paymentStatus: PaymentStatus;
+
+  @Prop({
+    required: true,
+  })
+  cardNumberLastFourDigits: string;
+
+  @Prop({
+    required: true,
+  })
+  amount: number;
+
+  @Prop({
+    required: true,
+  })
+  userEmail: string;
+}
+
+export const PaymentSchema = SchemaFactory.createForClass(PaymentDocument);
